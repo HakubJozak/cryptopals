82c2cf5545a7ab421105292396ccc3819644e08b
Jakub Hozak <jakub.hozak@gmail.com>
2018-12-06 09:49:51 +0100

    First excersise done.

--- 1.rb
+++ 1.rb
@@ -3,39 +3,28 @@
 require 'pry'
 require 'minitest'
 
-
-hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+source = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 target = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
+BASE64_CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"]
+
 def hex2bytes(str)
-  hex_digits = %w( 0 1 2 3 4 5 6 7 8 9 A B C D E F )
-  str.upcase.each_char.map { |c|
-    hex_digits.index(c)
+  str.upcase.scan(/.{2}/).map { |c|
+    c.to_i(16)
   }
 end
 
-
-BASE64_CHARS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"] 
-
 def bytes2base64(bytes)
-  bytes.each_slice(4) do |dword|
-    BASE64_CHARS.index(dword.sum)
-  end
+  binary = bytes.map { |b| ("%08b" % b).split('') }.flatten
+
+  binary.each_slice(6).map do |sextet|
+    value = sextet.join.to_i(2)
+    BASE64_CHARS[value]
+  end.join
 end
 
 
 
-
-
-
-bytes = hex2bytes(hex)
-require 'pry' ; binding.pry
-
-h = bytes2base64(bytes)
-
-
-#  assert_equal target, hex2base64(hex)
-
-
-
+bytes = hex2bytes(source)
+puts bytes2base64(bytes)
 
